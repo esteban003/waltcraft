f8b1380ba6e6a5d660c629a89bec3b7605b0c5aa
esteban003 <esteban_93150@hotmail.fr>
2018-06-14 21:31:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,12 +7,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === 'wc?help') {
-    msg.reply('embed=discord.Embed(title="le help n'est pas encore disponible", url="https://image.noelshack.com/fichiers/2018/24/4/1529004253-logo1.png")
-embed.set_author(name="WaltCraftBot", url="http://staff.waltcraft.eu/",, icon_url="https://image.noelshack.com/fichiers/2018/24/4/1529004253-logo1.png")
-embed.set_thumbnail(url="https://image.noelshack.com/fichiers/2018/24/4/1529004253-logo1.png")
-embed.add_field(name=TheDreamsRed, value=Copyright , inline=False)
-embed.set_footer(text="WaltCraftbot par TheDreamsRed")
-await self.bot.say(embed=embed)');
+    msg.reply('***help n"est pas encore disponible***
   }
 });
 client.login('NDA0MDMzNDQyMzM0OTAwMjI0.DgQCyw._IMh_h2J3v80pTmHBQrkwqXcE6Q');
