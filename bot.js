b5b3c0ae333fb4bfe1ea1a859bab790c3f858767
esteban003 <esteban_93150@hotmail.fr>
2018-06-14 17:56:38 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  if (msg.content === 'wc?help') {
+    msg.reply('le help n"est pas encore disponible');
   }
 });
 client.login('NDA0MDMzNDQyMzM0OTAwMjI0.DgQCyw._IMh_h2J3v80pTmHBQrkwqXcE6Q');
