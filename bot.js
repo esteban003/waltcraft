6c866043346b76aa8f70645d4b5f253c45cc1ec1
esteban003 <esteban_93150@hotmail.fr>
2018-06-14 23:02:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,10 +9,14 @@ client.on('message', msg => {
   if (msg.content === 'wc?help') {
     msg.reply('***Voici le help  wc?help : Voir les commande wc?close : Message pour serveur Indisponible wc?welcome : souhaité la bienvenue ***')
   }
+  
+});
 client.on('message', msg => {
   if (msg.content === 'wc?close') {
     msg.reply('***le serveur est actuellement en développement, nous n`avons pas de date pour le moment ***')
   }
+  
+});
 client.on('message', msg => {
   if (msg.content === 'wc?welcome') {
     msg.reply('***[FR] Bonjour et bienvenue sur le discord de WaltCraft [EN] Hello and welcome to the discord of WaltCraft***')
